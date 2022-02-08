2f21cab0b5ade53a7b0df3a0fd085809ed4fc22f
yokoi akinori <yokoiakinori@A112.local>
2022-02-09 01:47:42 +0900

    テストの追加

--- backend/tests/Feature/SubmitThumbnailTest.php
+++ backend/tests/Feature/SubmitThumbnailTest.php
@@ -86,4 +86,62 @@ class SubmitThumbnailTest extends TestCase
 
         $this->assertEmpty(Thumbnail::all());
     }
+
+    /**
+     * ユニークなファイル名が保存されているかのテスト
+     *
+     * @return void
+     */
+    public function test_filename_unique()
+    {
+        Storage::fake('s3');
+
+        $arr = [1, 2, 3, 4, 5];
+        foreach ($arr as $value){
+            $this->actingAs($this->users[$value])->post('/thumbnail',[
+                'thumbnail' => UploadedFile::fake()->image('photo.jpg')
+            ]);
+        }
+
+        $thumbnails = Thumbnail::get(['full_file_name']);
+
+        $keyRecord = $thumbnails->get(0);
+        $thumbnails->shift();
+
+        $this->assertNotContains($keyRecord, $thumbnails);
+    }
+
+    /**
+     * バリデーションエラーテスト（画像がない）
+     *
+     * @return void
+     */
+    public function test_validation_by_image_less()
+    {
+        Storage::fake('s3');
+
+        $response = $this->actingAs($this->users[1])->post('/thumbnail');
+
+        $response->assertStatus(302);
+
+        $this->assertEmpty(Thumbnail::all());
+    }
+
+    /**
+     * バリデーションエラーテスト（拡張子が正しくない）
+     *
+     * @return void
+     */
+    public function test_validation_by_no_suitable_image()
+    {
+        Storage::fake('s3');
+
+        $response = $this->actingAs($this->users[1])->post('/thumbnail',[
+            'thumbnail' => UploadedFile::fake()->image('photo.tiff')
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertEmpty(Thumbnail::all());
+    }
 }
