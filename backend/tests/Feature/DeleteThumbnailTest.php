cc7dc2f32229bbc4c02514b70d91b603ff7f27fd
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-02-13 01:16:43 +0900

    DB削除に失敗した場合のテスト実装

--- backend/tests/Feature/DeleteThumbnailTest.php
+++ backend/tests/Feature/DeleteThumbnailTest.php
@@ -61,4 +61,30 @@ class DeleteThumbnailTest extends TestCase
 
         $this->assertCount(0, Storage::disk('s3')->files());
     }
+
+    /**
+     * DB削除に失敗した場合の挙動のテスト
+     *
+     * @return void
+     */
+    public function test_submit_thumbnail_failure_by_db_error()
+    {
+        Storage::fake('s3');
+
+        $current = $this->actingAs($this->users[1])->post('/thumbnail',[
+            'thumbnail' => UploadedFile::fake()->image('photo.jpg')
+        ]);
+
+        Schema::table('thumbnails', function (Blueprint $table) {
+            $table->dropColumn('full_file_name');
+        });
+
+        $response = $this->actingAs($this->users[1])->delete('/thumbnail');
+
+        $response->assertStatus(500);
+
+        $thumbnail = Thumbnail::first();
+
+        $this->assertEquals($current['file_string'], $thumbnail['file_string']);
+    }
 }
