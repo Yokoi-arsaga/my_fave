b99f45da4d5f2bf6b6b366a2d25e934ea31d5579
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-02-15 01:16:05 +0900

    空文字列を送るとバリデーションで弾かれることを確認

--- backend/tests/Feature/EditProfileTest.php
+++ backend/tests/Feature/EditProfileTest.php
@@ -37,4 +37,25 @@ class EditProfileTest extends TestCase
         $response->assertStatus(200);
         $this->assertEquals($response['name'], $user['name']);
     }
+
+    /**
+     * 空文字列を送るとバリデーションで弾かれることを確認
+     *
+     * @return void
+     */
+    public function test_edit_profile_failure_by_empty()
+    {
+        $profileInfo = [
+            'name' => '',
+            'description' => 'よろしくお願いいたします',
+            'location' => '東京都'
+        ];
+
+        $response = $this->actingAs($this->user)->post('/profile/store', $profileInfo);
+
+        $user = User::first();
+
+        $response->assertRedirect('/');
+        $this->assertNull($user['description']);
+    }
 }
