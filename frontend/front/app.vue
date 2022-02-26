5d9b82041c1f39b1414993fc6af356b6e3a4aae7
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-02-26 10:32:31 +0900

    eslintインストール

--- frontend/front/app.vue
+++ frontend/front/app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <p>{{data}}</p>
-  </div>
+    <div>
+        <p>{{ data }}</p>
+    </div>
 </template>
 
 <script setup>
-    const {data} = await useFetch('http://my_fave_nginx:9000/api/check');
+const {data} = await useFetch('http://my_fave_nginx:9000/api/check');
 </script>
