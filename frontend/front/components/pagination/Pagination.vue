7562f3da4be0895c818b90707e8373a3ac34e42c
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-02-27 15:14:09 +0900

    ハリボテのページネーションコンポーネント作成

---
+++ frontend/front/components/pagination/Pagination.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page">
+        <ul>
+            <li v-for="n in 5">
+                {{ n }}
+            </li>
+        </ul>
+        <div>
+            <p>&<</p>
+            <p>{{this.currentPage}}</p>
+            <p>&></p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+interface PageInfo{
+    currentPage: number
+}
+export default {
+    data(): PageInfo{
+        return{
+            currentPage: 1
+        }
+    }
+}
+</script>
