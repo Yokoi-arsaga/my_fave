f0adfa998aa38ab54f6451e52e2c50cf1c93d337
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-03-02 10:47:06 +0900

    ユーザー登録ロジック修正

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,17 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
+use \Symfony\Component\HttpFoundation\Response;
 
 class RegisteredUserController extends Controller
 {
     /**
      * Display the registration view.
      *
-     * @return \Illuminate\View\View
+     * @return View
      */
     public function create()
     {
@@ -26,14 +30,14 @@ class RegisteredUserController extends Controller
     /**
      * Handle an incoming registration request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @return JsonResponse
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @throws ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        $request->validate([
+        $validation = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()
@@ -42,6 +46,10 @@ class RegisteredUserController extends Controller
                 ->numbers()
             ],
         ]);
+
+        if ($validation->fails()) {
+            return response()->json($validation->messages(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
         $user = User::create([
             'name' => $request->name,
@@ -53,6 +61,12 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        $json = [
+            'data' => $user,
+            'message' => 'User registration success!',
+            'error' => ''
+        ];
+
+        return response()->json( $json, Response::HTTP_OK);
     }
 }
