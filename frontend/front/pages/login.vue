f74774a171ff4c2043ead211b7d4e358fe8d65c8
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-03-05 22:03:36 +0900

    ログインフォームだけ実装

---
+++ frontend/front/pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>ログイン</h2>
+        <form>
+            <div class="form-group">
+                <label>Eメール</label>
+                <input type="text" aria-label="Eメール" v-model="form.email">
+            </div>
+            <div class="form-group">
+                <label>パスワード</label>
+                <input type="text" aria-label="Eメール" v-model="form.password">
+            </div>
+            <button type="submit" @click.prevent="submit">送信</button>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface FormData {
+    email: string,
+    password: string
+}
+
+const form:FormData = {
+    email: 'example.com',
+    password: 'secret'
+};
+</script>
+
