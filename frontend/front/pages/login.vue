a6f35cae5e922c32b4f8bcb7d066672a184614f6
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-03-07 01:59:26 +0900

    フォーム送信メソッドの実装

--- frontend/front/pages/login.vue
+++ frontend/front/pages/login.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="form-group">
                 <label>パスワード</label>
-                <input type="text" aria-label="Eメール" v-model="form.password">
+                <input type="text" aria-label="パスワード" v-model="form.password">
             </div>
             <button type="submit" @click.prevent="submit">送信</button>
         </form>
@@ -17,18 +17,20 @@
 
 <script setup lang="ts">
 import axios from "axios";
+
 interface FormData {
     email: string,
     password: string
 }
 
 const form:FormData = {
-    email: 'example.com',
-    password: 'secret'
+    email: 'yokoi4522-test1@gmail.com',
+    password: 'test'
 };
 
 const submit = async () => {
-    const user = await useFetch('http://my_fave_nginx:9000/api/check');
+    await axios.post('http://localhost:9000/api/login', form);
 }
+
 </script>
 
