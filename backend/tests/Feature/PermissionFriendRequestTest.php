4f18d6465f0a9a86dae4dd202af339d6e89584bc
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-03-20 19:47:43 +0900

    不要なコードの削除

--- backend/tests/Feature/PermissionFriendRequestTest.php
+++ backend/tests/Feature/PermissionFriendRequestTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Models\FriendRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\FriendRequest;
+use App\Models\Friend;
 
 class PermissionFriendRequestTest extends TestCase
 {
@@ -63,68 +64,5 @@ class PermissionFriendRequestTest extends TestCase
 
         $user = User::find($this->users[1]->id);
         $this->assertEmpty($user->friends);
-    }
-
-    /**
-     * 送信先IDが空欄だった場合バリデーションで弾かれることを確認
-     *
-     * @return void
-     */
-    public function test_store_friend_request_failure_by_destination_empty()
-    {
-        $friendRequestInfo = [
-            'destination_id' => '',
-            'message' => 'よろしくお願いいたします。',
-        ];
-
-        $response = $this->actingAs($this->users[1])->post('/api/friend/request/store', $friendRequestInfo);
-
-        $response->assertRedirect('/');
-        $this->assertEmpty(FriendRequest::all());
-
-        $user = User::find($this->users[2]->id);
-        $this->assertEmpty($user->notifications);
-    }
-
-    /**
-     * 送信先IDが自身だった場合リダイレクトされることを確認
-     *
-     * @return void
-     */
-    public function test_store_friend_request_failure_by_destination_mine()
-    {
-        $friendRequestInfo = [
-            'destination_id' => $this->users[1]->id,
-            'message' => 'よろしくお願いいたします。',
-        ];
-
-        $response = $this->actingAs($this->users[1])->post('/api/friend/request/store', $friendRequestInfo);
-
-        $response->assertRedirect('/');
-        $this->assertEmpty(FriendRequest::all());
-
-        $user = User::find($this->users[2]->id);
-        $this->assertEmpty($user->notifications);
-    }
-
-    /**
-     * 送信先IDが存在しない場合リダイレクトされることを確認
-     *
-     * @return void
-     */
-    public function test_store_friend_request_failure_by_destination_not_exist()
-    {
-        $friendRequestInfo = [
-            'destination_id' => 3,
-            'message' => 'よろしくお願いいたします。',
-        ];
-
-        $response = $this->actingAs($this->users[1])->post('/api/friend/request/store', $friendRequestInfo);
-
-        $response->assertRedirect('/');
-        $this->assertEmpty(FriendRequest::all());
-
-        $user = User::find($this->users[2]->id);
-        $this->assertEmpty($user->notifications);
     }
 }
