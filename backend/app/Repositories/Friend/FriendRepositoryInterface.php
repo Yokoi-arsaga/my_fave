c60c2aa96400c461bbe3d22879bbd1a8945b2015
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-03-20 23:44:37 +0900

    FriendRepositoryを作成

---
+++ backend/app/Repositories/Friend/FriendRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories\Friend;
+
+use App\Models\Friend;
+use Illuminate\Http\Request;
+
+/**
+ * interface SnsAccountRepository SNSアカウントの処理
+ * @package App\Repositories\SnsAccountRepository
+ */
+interface FriendRepositoryInterface
+{
+    /**
+     * インサート
+     *
+     * @param int $applicantId
+     * @return Friend
+     */
+    public function storeFriend(int $applicantId): Friend;
+}
