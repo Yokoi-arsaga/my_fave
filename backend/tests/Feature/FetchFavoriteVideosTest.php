dfad6f8203113b96c8832c7066f7f4815418172c
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-03-24 23:34:54 +0900

    認証していない場合にリダイレクトされる

--- backend/tests/Feature/FetchFavoriteVideosTest.php
+++ backend/tests/Feature/FetchFavoriteVideosTest.php
@@ -47,7 +47,6 @@ class FetchFavoriteVideosTest extends TestCase
             $this->actingAs($this->users[1])->post('/api/favorite/videos/store', $videoInfo);
         }
 
-
         $response = $this->actingAs($this->users[1])->get('/api/favorite/videos/fetch');
 
         $response->assertStatus(200);
@@ -55,56 +54,33 @@ class FetchFavoriteVideosTest extends TestCase
     }
 
     /**
-     * 動画URLが空欄だった場合バリデーションで弾かれることを確認
+     * お気に入り動画一覧の取得に成功したテスト
      *
      * @return void
      */
-    public function test_store_favorite_video_failure_by_url_empty()
+    public function test_fetch_favorite_videos_failure_by_not_auth()
     {
-        $favoriteVideoInfo = [
-            'video_url' => '',
-            'video_name' => 'サンプル',
+        $favoriteVideoInfos = [
+            [
+                'video_url' => 'https://www.youtube.com/watch?v=NwOvu-j_WjY',
+                'video_name' => 'サンプル1',
+            ],
+            [
+                'video_url' => 'https://www.youtube.com/watch?v=oq20w95OlSY',
+                'video_name' => 'サンプル2',
+            ],
+            [
+                'video_url' => 'https://www.youtube.com/watch?v=AElYX7XQ-7s',
+                'video_name' => 'サンプル3',
+            ],
         ];
 
-        $response = $this->actingAs($this->users[1])->post('/api/favorite/videos/store', $favoriteVideoInfo);
+        foreach ($favoriteVideoInfos as $videoInfo){
+            $this->actingAs($this->users[1])->post('/api/favorite/videos/store', $videoInfo);
+        }
+
+        $response = $this->get('/api/favorite/videos/fetch');
 
         $response->assertRedirect('/');
-        $this->assertEmpty(FavoriteVideo::all());
-    }
-
-    /**
-     * 動画名が空欄だった場合バリデーションで弾かれることを確認
-     *
-     * @return void
-     */
-    public function test_store_favorite_video_failure_by_name_empty()
-    {
-        $favoriteVideoInfo = [
-            'video_url' => 'https://www.youtube.com/watch?v=NwOvu-j_WjY',
-            'video_name' => '',
-        ];
-
-        $response = $this->actingAs($this->users[1])->post('/api/favorite/videos/store', $favoriteVideoInfo);
-
-        $response->assertRedirect('/');
-        $this->assertEmpty(FavoriteVideo::all());
-    }
-
-    /**
-     * 動画URLがyoutube動画の形式でない場合バリデーションで弾かれることを確認
-     *
-     * @return void
-     */
-    public function test_store_favorite_video_failure_by_format_different()
-    {
-        $favoriteVideoInfo = [
-            'video_url' => 'https://www.arsaga.jp/',
-            'video_name' => 'サンプル',
-        ];
-
-        $response = $this->actingAs($this->users[1])->post('/api/favorite/videos/store', $favoriteVideoInfo);
-
-        $response->assertRedirect('/');
-        $this->assertEmpty(FavoriteVideo::all());
     }
 }
