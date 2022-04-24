a5e18f98202efabca9ee24db4ac79844b9a0b7b3
Akinori Yokoi <yokoi_akinori@arsaga.jp>
2022-04-24 20:28:06 +0900

    テスト実装

---
+++ backend/tests/Feature/ChangeDisclosureRangeByParentFolderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ParentFolder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+
+class ChangeDisclosureRangeByParentFolderTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $arr = [1, 2];
+        foreach ($arr as $value) {
+            $this->users[$value] = User::factory()->create();
+        }
+    }
+
+    /**
+     * 親フォルダーの公開範囲の変更に成功したテスト
+     *
+     * @return void
+     */
+    public function test_change_disclosure_range_by_parent_folder_success()
+    {
+        $parentFolderInfo = [
+            'folder_name' => 'サンプル',
+            'description' => '動画フォルダーの説明文',
+            'disclosure_range_id' => 1,
+            'is_nest' => false
+        ];
+
+        $parentFolder = $this->actingAs($this->users[1])->post('/api/favorite/folder/parent/store', $parentFolderInfo);
+        $parentFolderId = $parentFolder['id'];
+        $disclosureRangeId = ['disclosure_range_id' => 2];
+
+        $response = $this->actingAs($this->users[1])->patch("/api/favorite/folder/parent/disclosure/$parentFolderId", $disclosureRangeId);
+
+        $response->assertStatus(200);
+        $this->assertEquals($response['disclosure_range_id'], $disclosureRangeId);
+    }
+
+    /**
+     * 公開範囲が無効だった場合バリデーションで弾かれることを確認
+     *
+     * @return void
+     */
+    public function test_store_parent_folder_failure_by_disclosure_out_of_range()
+    {
+        $parentFolderInfo = [
+            'folder_name' => 'サンプル',
+            'description' => '動画フォルダーの説明文',
+            'disclosure_range_id' => 1,
+            'is_nest' => false
+        ];
+
+        $parentFolder = $this->actingAs($this->users[1])->post('/api/favorite/folder/parent/store', $parentFolderInfo);
+        $parentFolderId = $parentFolder['id'];
+        $disclosureRangeId = ['disclosure_range_id' => 4];
+
+        $response = $this->actingAs($this->users[1])->patch("/api/favorite/folder/parent/disclosure/$parentFolderId", $disclosureRangeId);
+
+        $response->assertRedirect('/');
+
+        $this->assertEquals($response['disclosure_range_id'], $parentFolderInfo['disclosure_range_id']);
+    }
+}
